ac639e627a781608fdeea4a1c453ff918a9eb8cc
rs9211823397-ship-it <rs9211823397@gmail.com>
2026-07-23 12:19:10 +0000

    Improved signal engine scoring logic

--- strategy/signal_engine.py
+++ strategy/signal_engine.py
@@ -1,42 +1,35 @@
-"""
-Trading Signal Engine
-Generates BUY / SELL / HOLD decisions.
-"""
-
-
 class SignalEngine:
 
     def generate_signal(self, data):
 
         latest = data.iloc[-1]
 
-        rsi = latest["RSI"]
-        macd = latest["MACD"]
-        macd_signal = latest["MACD_SIGNAL"]
-        ema20 = latest["EMA_20"]
-        ema50 = latest["EMA_50"]
-        close = latest["Close"]
+        score = 0
 
-        # BUY conditions
-        if (
-            rsi < 70
-            and macd > macd_signal
-            and ema20 > ema50
-            and close > ema20
-        ):
+        if latest["Close"] > latest["EMA_20"]:
+            score += 1
+        else:
+            score -= 1
+
+        if latest["EMA_20"] > latest["EMA_50"]:
+            score += 1
+        else:
+            score -= 1
+
+        if latest["RSI"] < 35:
+            score += 1
+        elif latest["RSI"] > 65:
+            score -= 1
+
+        if latest["MACD"] > latest["MACD_SIGNAL"]:
+            score += 1
+        else:
+            score -= 1
+
+        if score >= 2:
             return "BUY"
 
-
-        # SELL conditions
-        elif (
-            rsi > 30
-            and macd < macd_signal
-            and ema20 < ema50
-            and close < ema20
-        ):
+        elif score <= -2:
             return "SELL"
 
-
-        # No strong setup
-        else:
-            return "HOLD"
+        return "HOLD"
