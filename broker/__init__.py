71e0c8d7bf43d4c89b2edfc5622a7af9d8d3c277
rs9211823397-ship-it <rs9211823397@gmail.com>
2026-07-29 04:05:04 +0530

    Export MT5 broker adapter

--- broker/__init__.py
+++ broker/__init__.py
@@ -1,4 +1,5 @@
 from broker.base import Broker, BrokerOrder, OrderStatus
+from broker.mt5_broker import MT5Broker
 from broker.paper_broker import PaperBroker
 
-__all__ = ["Broker", "BrokerOrder", "OrderStatus", "PaperBroker"]
+__all__ = ["Broker", "BrokerOrder", "OrderStatus", "PaperBroker", "MT5Broker"]
