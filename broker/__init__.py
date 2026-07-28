1e0e995acc693bc5be7019079275666eedd2dd3a
rs9211823397-ship-it <rs9211823397@gmail.com>
2026-07-29 04:06:14 +0530

    Expose safe broker factory

--- broker/__init__.py
+++ broker/__init__.py
@@ -1,5 +1,13 @@
 from broker.base import Broker, BrokerOrder, OrderStatus
+from broker.factory import create_broker
 from broker.mt5_broker import MT5Broker
 from broker.paper_broker import PaperBroker
 
-__all__ = ["Broker", "BrokerOrder", "OrderStatus", "PaperBroker", "MT5Broker"]
+__all__ = [
+    "Broker",
+    "BrokerOrder",
+    "OrderStatus",
+    "PaperBroker",
+    "MT5Broker",
+    "create_broker",
+]
