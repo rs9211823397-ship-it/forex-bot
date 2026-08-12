7574e43db7a63a2e499bab1b9e2e0b5374544f7f
rs9211823397-ship-it <rs9211823397@gmail.com>
2026-08-12 02:35:48 +0000

    test analytics launcher settings

--- tests/test_ai_chart_activation.py
+++ tests/test_ai_chart_activation.py
@@ -97,6 +97,9 @@ def test_windows_launcher_uses_capture_only_observer_mode():
     assert 'AAQTS_AI_CHART_REMOTE_ENABLED = "false"' in launcher
     assert 'AAQTS_AI_CHART_OUTCOMES_ENABLED = "true"' in launcher
     assert 'AAQTS_AI_CHART_OUTCOME_HORIZONS = "1,3,6,12"' in launcher
+    assert 'AAQTS_AI_CHART_ANALYTICS_ENABLED = "true"' in launcher
+    assert 'AAQTS_AI_CHART_ANALYTICS_MIN_FINALIZED = "30"' in launcher
+    assert 'AAQTS_AI_CHART_ANALYTICS_MIN_BUCKET = "10"' in launcher
     assert "Remove-Item Env:OPENAI_API_KEY" in launcher
     assert "ConvertTo-SecureString" in launcher
     assert "ConvertFrom-SecureString" in setter
